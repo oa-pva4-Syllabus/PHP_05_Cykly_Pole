06fbb068677f93d7aea2109a751a1d5b6d3b5108
Adam Fišer <adam.fiser@wanex.cz>
2022-11-03 09:50:38 +0100

    Update pole_mesic.php

--- pole_mesic.php
+++ pole_mesic.php
@@ -2,6 +2,8 @@
 /**
 * Asociativní pole
 * Deklarujte pole months, které bude mít v hodnotách uložen český název měsíce. Index bude odpovídat pořadovému číslu měsíce.
+* a) Za pomocí cyklu zobrazte všechny měsíce.
+* b) Za pomocí cyklu zobrazte všechny měsíce z období 6-12.
 */
 
 //reseni
